ec3a2fd87355e36b10f8c1bcd7e8cfbbac5e29c1
KobeyArai808 <kobeyarai@gmail.com>
2021-11-18 12:30:04 -1000

    Implemented a few test cases to test all() method.

--- src/edu/ics211/h10/DoubleBinarySearchTree.java
+++ src/edu/ics211/h10/DoubleBinarySearchTree.java
@@ -5,17 +5,42 @@ public class DoubleBinarySearchTree implements DoubleBinarySearchTreeInterface{
     private MyBinaryTreeNode head;
 
     public static void main(String[] args) {
-        DoubleBinarySearchTree testing = new DoubleBinarySearchTree(new double[]{4.0, 3.0, 5.0});
+        /*DoubleBinarySearchTree testing = new DoubleBinarySearchTree(new double[]{4.0, 3.0, 5.0});
         testing.add(1.0);
         testing.add(2.0);
-        System.out.println(testing.head.getValue());
+        System.out.println(testing.head.getValue()+" is the head");
         System.out.println(testing.head.getLeftRef().getLeftRef().getValue());
         System.out.println(testing.head.getLeftRef().getLeftRef().getRightRef().getValue());
+        double [] testingArr = testing.all();
+        for (int i = 0; i < testingArr.length; i++ ) {
+            System.out.print(testingArr[i] + " ");
+        }*/
+
+
+        System.out.println("\n\n");
+        DoubleBinarySearchTree created = new DoubleBinarySearchTree();
+        created.add(10.0);
+        created.add(2.0);
+        created.add(3.0);
+        created.add(4.0);
+        created.add(6.0);
+        created.add(5.0);
+        System.out.println("\n\n");
+        System.out.println(created.head.getValue()+" is the head");
+        System.out.println(created.head.getLeftRef().getValue());
+        System.out.println(created.head.getLeftRef().getRightRef().getValue());
+        System.out.println(created.head.getLeftRef().getRightRef().getRightRef().getValue());
+        System.out.println(created.head.getLeftRef().getRightRef().getRightRef().getRightRef().getValue());
+        System.out.println(created.head.getLeftRef().getRightRef().getRightRef().getRightRef().getLeftRef().getValue());
+        double [] stored = created.all();
+        for (int i = 0; i < stored.length; i++ ) {
+            System.out.print(stored[i] + " ");
+        }
+        System.out.print(created.depth());
     }
 
     // constructor, creates an empty binary search tree
     public DoubleBinarySearchTree(){
-
     }
 
     // constructor, creates a binary search tree containing all the given values
@@ -28,6 +53,7 @@ public class DoubleBinarySearchTree implements DoubleBinarySearchTreeInterface{
 
     @Override
     public boolean add(double value) {
+        if(head==null) head = new MyBinaryTreeNode(value);
         MyBinaryTreeNode theNode = helperAdd(head, value);
         return theNode!=null;
     }
@@ -35,7 +61,12 @@ public class DoubleBinarySearchTree implements DoubleBinarySearchTreeInterface{
     @Override
     public MyBinaryTreeNode helperAdd(MyBinaryTreeNode root, double value){
         MyBinaryTreeNode valNode = new MyBinaryTreeNode(value);
-        if(value>root.getValue()){
+        if(root==null){
+            //System.out.println(valNode.getValue());
+            root = valNode;
+            return root;
+        }
+        else if(value>root.getValue()){
             if(!root.hasRightRef()){
                 root.addRightRef(valNode);
                 return valNode;
@@ -107,7 +138,7 @@ public class DoubleBinarySearchTree implements DoubleBinarySearchTreeInterface{
 
     @Override
     public double[] all() {
-        return allHelper(new double[10], head, countNodes(head));
+        return allHelper(new double[countNodes(head)], head, 0);
     }
 
     public double[] allHelper(double[] arr, MyBinaryTreeNode head, int index){
@@ -115,9 +146,11 @@ public class DoubleBinarySearchTree implements DoubleBinarySearchTreeInterface{
 
         }
         else{
+            System.out.println("Thing iterating next: "+head.getLeftRef());
+            System.out.println("Thing iterating next: "+head.getRightRef());
             allHelper(arr, head.getLeftRef(),index);
-            arr[index+1] = head.getValue();
-            allHelper(arr, head.getRightRef(),index+2);
+            arr[index++] = head.getValue();
+            allHelper(arr, head.getRightRef(),index);
         }
         return arr;
     }
